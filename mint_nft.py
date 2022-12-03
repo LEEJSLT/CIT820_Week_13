6ce1f788f1412d64d09c5fccf7c7c95b2650fddb
Taoli93 <48353960+LEEJSLT@users.noreply.github.com>
2022-12-03 22:37:17 +0800

    Update mint_nft.py

--- mint_nft.py
+++ mint_nft.py
@@ -72,13 +72,12 @@ def mint_nft(nft_contract,tokenId,metadata,owner_address,minter_address):
 
 		'file': json.dumps(metadata)
 	}
-	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files)#, auth=(project_id, project_secret))
-	response = response.json()
-	cid = response["Hash"]
+	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=(project_id, project_secret))
+	# response = response.json()
+	# cid = response["Hash"]
 
-	# print(response.text)
-
-	# cid = response.text.split(",")[1].split(":")[1].replace('"','')
+	print(response.text)
+	cid = response.text.split(",")[1].split(":")[1].replace('"','')
 	# cid = response["Hash"]
 
 	# print(cid)
